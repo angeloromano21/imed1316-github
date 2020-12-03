021e37655cc41c5c5dfc27705e6ce75ad0e884e8
angeloromano21 <70916951+angeloromano21@users.noreply.github.com>
2020-12-02 18:56:01 -0800

    Dragged and Dropped Files

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+  
+         //carousel
+        $('.carousel').carousel({
+        interval: 550
+      })
+  
+  //AOS
+  AOS.init();
+  
+});
